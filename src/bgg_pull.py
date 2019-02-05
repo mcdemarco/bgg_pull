e84b15f3970793e993cd4ed5bb47faf3996c85b3
M. C. DeMarco <>
2019-02-04 21:30:08 -0500

    Stop using unreliable type mismatch attribute.

--- src/bgg_pull.py
+++ src/bgg_pull.py
@@ -47,13 +47,16 @@ tag_col_lookup = [('name', 'names'),
                  ('boardgamecategory', 'category'),
                  ('boardgamedesigner', 'designer'),
                  ('boardgamepublisher', 'publisher'),
-                 ('statistics/ratings/averageweight', 'weight')]
+                 ('statistics/ratings/averageweight', 'weight'),
+                 ('boardgameexpansion', 'expands'),
+                 ('boardgameimplementation', 'reimplements')]
 
 full_col_list = ['game_id','bgg_url']
 for tag, col in tag_col_lookup:
     full_col_list.append(col)
 
 current = 1
+
 
 def GetFromApi(max=271300, loops=100, tags_cols=tag_col_lookup):
     '''
@@ -65,13 +68,8 @@ def GetFromApi(max=271300, loops=100, tags_cols=tag_col_lookup):
     '''
 
     global current
-    path = os.path.join(args.out_path, args.out_name)
-    df = pandas.DataFrame([], columns = full_col_list)
-    if os.path.isfile(path):
-        # search through df for max game id and start from there.
-        df = pandas.read_csv(path, encoding='utf8', float_precision='high')
-        if int(df.iloc[-1,0]) >= current:
-            current = int(df.iloc[-1,0]) + 1
+    global df
+    global path
  
     ids_todo = []
     for index in range(current,current + loops):
@@ -81,20 +79,35 @@ def GetFromApi(max=271300, loops=100, tags_cols=tag_col_lookup):
             ids_todo.append(str(index))
 
     current = current + loops
+    lendf = len(df)
 
-    url = 'https://www.boardgamegeek.com/xmlapi/boardgame/{}?&stats=1&marketplace=1'.format(','.join(ids_todo))
+    url = 'https://www.boardgamegeek.com/xmlapi/boardgame/{}?&stats=1'.format(','.join(ids_todo))
     args.logger.info(f'Grabbing info from {url}')
+    args.logger.info(f'Starting with {lendf} games')
     response = requests.get(url)
     if response.status_code != 200:
         args.logger.error(f'Problem grabbing from API:  {response.status_code}')
         sys.exit(1)
+    content = response.content
+    #clean the response
+    if content.find(b'<') >= 1:
+        args.logger.error('Problem grabbing from API: response included dots')
+    content = content[content.find(b'<'):]
 
     # these tags will return multiple results, will need to be handled slightly differently
     multi_tags = ['mechanic', 'category', 'designer']
-    tree = ElementTree.fromstring(response.content)
+    tree = ElementTree.fromstring(content)
+    #some debugging
+    elementCount = 0
+    rowCount = 0
     for game in tree:
-        if 'objectid' in game.attrib and not 'subtypemismatch' in game.attrib:
+        elementCount += 1
+        if 'objectid' in game.attrib and game.find('minplayers').text:
             id = game.attrib['objectid']
+            if id == 41047:
+                # omit a test item.  138691 is also a test item but won't be picked up because it has no minplayers
+                break
+            rowCount += 1
             df_dict = {'game_id':id, 'bgg_url':"https://boardgamegeek.com/boardgame/" + str(id)}
             for tag, var in tags_cols:
                 # special case for grabbing english name
@@ -103,11 +116,22 @@ def GetFromApi(max=271300, loops=100, tags_cols=tag_col_lookup):
                         if 'primary' in sub.attrib: #grab the english name
                             df_dict[var] = sub.text if sub != None else 'none'
                             break
-                # special case for grabbing rank (when not scraping)
+                # special case for grabbing rank (without scraping)
                 elif var == 'rank':
                     for sub in game.findall(tag):
-                        if 'id' in sub.attrib and sub.attrib["id"] == '1': #grab the rank
-                            df_dict[var] = sub.attrib["value"] if sub != None and sub.attrib["value"] != 'Not Ranked' else 'none'
+                        if 'id' in sub.attrib and sub.attrib["id"] == '1' and sub.attrib["value"] != 'Not Ranked': #grab the rank
+                            df_dict[var] = sub.attrib["value"] if sub != None else 'none'
+                            break
+                # special case for expansions
+                elif var == 'expands':
+                    for sub in game.findall(tag):
+                        if 'inbound' in sub.attrib: #grab the game
+                            df_dict[var] = sub.attrib["objectid"] if sub != None else 'none'
+                            break
+                elif var == 'reimplements':
+                    for sub in game.findall(tag):
+                        if 'inbound' in sub.attrib: #grab the game
+                            df_dict[var] = sub.attrib["objectid"] if sub != None else 'none'
                             break
                 # multi tag items need to be handled slightly different
                 elif var in multi_tags:
@@ -122,25 +146,26 @@ def GetFromApi(max=271300, loops=100, tags_cols=tag_col_lookup):
 
             df = df.append(df_dict, ignore_index = True)
 
-        else:
+        
             # check for a game not found error (vs. some other error)
-            for sub in game.findall('error'):
-                args.logger.info(sub)
-                if 'message' in sub.attrib:
-                    args.logger.info( sub.attrib["message"] )
-                else:
-                    args.logger.info('The server returned an error.')
+            #for sub in game.findall('error'):
+            #    args.logger.info(sub)
+            #    if 'message' in sub.attrib:
+            #        args.logger.info( sub.attrib["message"] )
+            #    else:
+            #        args.logger.info('The server returned an error.')
                     
     # save results out
-    args.logger.info('Saving...')
-    path = os.path.join(args.out_path, args.out_name)
-    df.to_csv(path, index=False)
+    args.logger.info(f'Saving {rowCount} new games out of {elementCount} received...')
+    if rowCount >= 1:
+        df.to_csv(path, index=False)
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Collects information for games on BGG')
     parser.add_argument('-a', '--api', dest='api_grab', type=int, default=271300, help='Maximum game id to grab, keep doing this until db full')
+    parser.add_argument('-c', '--current', dest='api_current', type=int, default=1, help='Minimum game id to grab (useful for skipping bum IDs)')
     args = parser.parse_args()
 
     # add extra useful stuff
@@ -173,6 +198,19 @@ if __name__ == '__main__':
     if not 1 <= args.api_grab <= 500000:
         args.logger.error('invalid value for api_grabs [1, 500000]')
         sys.exit(1)
+    if not 1 <= args.api_current <= 500000:
+        args.logger.error('invalid value for api_current [1, 500000]')
+        sys.exit(1)
+    else: 
+        current = args.api_current
+
+    path = os.path.join(args.out_path, args.out_name)
+    df = pandas.DataFrame([], columns = full_col_list)
+    if os.path.isfile(path):
+        # search through df for max game id and start from there.
+        df = pandas.read_csv(path, encoding='utf8', float_precision='high')
+        if int(df.iloc[-1,0]) >= current:
+            current = int(df.iloc[-1,0]) + 1
     
     while current < args.api_grab:
         args.logger.info('Calling API...')
